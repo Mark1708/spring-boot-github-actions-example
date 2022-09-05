b72cedf2d0c4327f6a6db9a86cc7c33341f535dd
Марк Гурьянов <mark1708.work@gmail.com>
2022-09-05 14:18:01 +0300

    edit test

---
+++ src/test/java/com/example/springbootapp/person/PersonServiceTest.java
@@ -0,0 +1,59 @@
+package com.example.springbootapp.person;
+
+import com.example.springbootapp.utils.PersonServiceUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.springbootapp.utils.PersonServiceUtils.PERSON_1;
+import static com.example.springbootapp.utils.PersonServiceUtils.PERSON_2;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+    @Mock
+    private PersonRepository repo;
+
+    private PersonService service;
+
+    private List<Person> persons;
+
+    @BeforeEach
+    void init() {
+        service = new PersonService(repo);
+        PersonServiceUtils.initData();
+        persons = new ArrayList<>();
+        persons.add(PERSON_1);
+        persons.add(PERSON_2);
+        repo.saveAll(persons);
+    }
+
+    @Test
+    void getAllPerson() {
+        Mockito.when(repo.findAll()).thenReturn(persons);
+        List<Person> result = service.getAllPerson();
+        assertEquals(2, result.size());
+        assertTrue(result.contains(PERSON_1));
+        assertTrue(result.contains(PERSON_2));
+    }
+
+    @Test
+    void getPerson() {
+        Mockito.when(repo.findPersonById(1L)).thenReturn(Optional.of(PERSON_1));
+        assertEquals(PERSON_1, service.getPerson(1L));
+    }
+
+    @Test
+    void getPersonException() {
+        Mockito.when(repo.findPersonById(2L)).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> service.getPerson(2L));
+    }
+}
