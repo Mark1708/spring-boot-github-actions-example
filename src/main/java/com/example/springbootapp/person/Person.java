f7c7c02cd50f72097fd413def4de3ce41b3a5e54
Марк Гурьянов <mark1708.work@gmail.com>
2022-09-05 23:04:58 +0300

    modify Person

--- src/main/java/com/example/springbootapp/person/Person.java
+++ src/main/java/com/example/springbootapp/person/Person.java
@@ -1,13 +1,10 @@
 package com.example.springbootapp.person;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.*;
-import java.util.Objects;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "people")
 public class Person {
@@ -19,25 +16,4 @@ public class Person {
     private String name;
     private String role;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Person)) return false;
-        Person person = (Person) o;
-        return getId().equals(person.getId()) && getName().equals(person.getName()) && getRole().equals(person.getRole());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getRole());
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", role='" + role + '\'' +
-                '}';
-    }
 }
